4d491c2dc9068c5bd48ce421bc3089420a4d3343
Faisal Husain Shaikh <faisalhs2002@gmail.com>
2023-09-27 16:51:32 +0530

    footer and contact form added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let menu = () => {
+  if (window.innerWidth > 600) return;
   let disp = document.getElementsByTagName("header")[0];
 
   if (disp.style.display === "none" || disp.style.display === "") {
@@ -12,3 +13,29 @@ let menu = () => {
     document.getElementsByClassName("close-button")[0].style.display = "none";
   }
 };
+
+let scroll_to_top = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll("nav a");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      const targetId = link.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        window.scrollTo({
+          top: targetElement.offsetTop,
+          behavior: "smooth",
+        });
+      }
+    });
+  });
+});
